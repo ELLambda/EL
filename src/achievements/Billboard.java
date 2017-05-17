7c186250a352d2874b91b144faf9adaef464ddb7
文向东\wenxi <161250060@smail.nju.edu.cn>
2017-05-17 21:23:11 +0800

    nothing

---
+++ src/achievements/Billboard.java
@@ -0,0 +1,76 @@
+package achievements;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**  
+* 无尽模式的高分榜
+* 
+*  
+* @author Andy
+* @version  
+*/
+
+public class Billboard
+{
+	//榜单上记录的排位个数
+	private static final int RANK = 10;
+	
+	//榜单
+	//按照分数高低排序
+	//如果游戏次数少于rank的数目，存空串
+	
+	//用长度为rank+1的数组存，每次新存的数据放到最后一位，GUI上只显示前10位
+	static BillboardItem[] scorelist= new BillboardItem[RANK + 1];
+	
+	
+	//读高分榜
+	public static void getBillboardCondition() {
+		String line = new String();
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(
+					"src/achievements/Billboard.txt"));
+
+			if((line = br.readLine()) != null){
+				
+
+
+			String[] spl = line.split("@");
+			
+
+			for (int i = 0; i < RANK; i++) {
+				scorelist[i].setBillboardItem(spl[i]);
+			
+
+			}
+			}
+			
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//存高分榜
+	public static void setBillboardCondition() {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(
+					"src/achievements/Billboard.txt",false));
+			for(int i=0;i<RANK-1;i++){
+				bw.write(scorelist[i]+"@");
+			}
+			bw.write(scorelist[RANK-1]+"");
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+}
---
+++ src/achievements/BillboardItem.java
@@ -0,0 +1,72 @@
+package achievements;
+
+import java.util.Comparator;
+
+/**  
+* 高分榜中的一条高分信息
+* 实现了comparator方法
+*  
+* @author Andy
+* @version  
+*/
+
+public class BillboardItem implements Comparable<Object>
+{
+	//分数
+	public int score;
+	//时间
+	public int time;
+
+//	public int getScore()
+//	{
+//		return score;
+//	}
+//
+//	public void setScore(int score)
+//	{
+//		this.score = score;
+//	}
+//
+//	public int getTime()
+//	{
+//		return time;
+//	}
+//
+//	public void setTime(int time)
+//	{
+//		this.time = time;
+//	}
+
+	
+	//构造体，获取高分信息
+	public void setBillboardItem(String input){
+		if(input != null){
+			//文件中用&分隔了时间和分数
+			//时间在前分数在后
+			String[] temp = input.split("&");
+			this.time = Integer.valueOf(temp[0]);
+			this.score = Integer.valueOf(temp[1]);
+			
+			}
+	}
+	
+	public String getBillboardItem(){
+		return this.time + "&" + this.score;
+	}
+	
+
+	//降序排列
+	@Override
+	public int compareTo(Object o)
+	{
+		// TODO Auto-generated method stub
+		if(this.score >((BillboardItem)o).score)
+			return -1;
+		else
+			return 1;
+		
+	}
+
+
+	
+}
