303c19385c7c2b1ecc949e0bd1ed590e0842eb20
文向东\wenxi <161250060@smail.nju.edu.cn>
2017-05-18 00:29:25 +0800

    完成了AchievementWin 相关逻辑明天再弄 good n8

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application{
 				// TODO Auto-generated method stub
 				try {
 					new WelcomeWin().start(new Stage());
+					new AchievementWin();
 //					Data.setLimit(12);
 //					new GameWin();
 				} catch (Exception e) {
