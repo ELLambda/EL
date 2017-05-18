b45a4433444c39ad8c259a68634bce8ff41b4404
161250055 <161250055@mail.smal.nju.edu.cn>
2017-05-18 13:24:56 +0800

    完善商店

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -20,5 +20,46 @@ public class Shop
 	//构造体顺序为名字，价格，添加的步数，添加的分数
 	list.add(new ShopItem("",0,0,0));
 	
+	
+	
 	}
+	
+	//上一关获得的分数在上一关的界面中改成金币数
+	//上一关结束的时候把分数转为金币数
+	public int coins;
+	
+	
+	//被选择的商品清单，后期可以实现背包或队伍在剧情主关卡的可视化
+	ArrayList<ShopItem> selectedList = new ArrayList<ShopItem>();
+	
+	//允许购买的数目
+	public static final int SIZE_LIMIT = 2;
+
+	//上一关结束后实例化一个商店
+	public Shop(int coins){
+		this.coins = coins;
+	}
+	
+	//购买此商品
+	public void buy(ShopItem s){
+		if(selectedList.size()<=SIZE_LIMIT)
+		if(s.isSelected()&&s.isAffordable(coins))
+		{
+			coins-=s.getPrice();
+			selectedList.add(s);
+		}
+		}
+	
+	//不再购买此商品	
+	public void regret(ShopItem s){
+		if(s.isSelected()){
+			coins+=s.getPrice();
+			selectedList.remove(s);
+		}
+				
+		
+	}
+	
+	
+	
 }
